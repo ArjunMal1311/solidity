0688e4db56079764d91d071097d3ad79b5e13f77
Arjun Malhotra <94740065+ArjunMal1311@users.noreply.github.com>
2025-02-06 11:13:45 +0530

    Update DecentralizedStableCoin.sol

--- defi/src/DecentralizedStableCoin.sol
+++ defi/src/DecentralizedStableCoin.sol
@@ -6,27 +6,33 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    // We are using ERC20Burnable because it has burn function and thus we need to use its constructor as well
+    // Ownable grants exclusive control over specific functions to the owner of the contract
+
+    // ERC-20 token standard implementation, which includes basic functions like transferring tokens, checking balances, and approving transfers.
+
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
 
+    // permanently removing the NFT from the circulation
     function burn(uint256 amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (amount <= 0) revert DecentralizedStableCoin__MustBeMoreThanZero();
-        if (amount > balance)
-            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
-        super.burn(amount);
+        if (amount > balance) revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        
+        super.burn(amount); // run the function of parent class
     }
 
-    function mint(
-        address to,
-        uint256 amount
-    ) external onlyOwner returns (bool) {
+
+    // Creating and registering new NFT to blockchain
+    function mint(address to, uint256 amount) external onlyOwner returns (bool) {
         if (to == address(0)) revert DecentralizedStableCoin__NotZeroAddress();
         if (amount <= 0) revert DecentralizedStableCoin__MustBeMoreThanZero();
-        _mint(to, amount);
+        
+        _mint(to, amount); // inherited from ERC20 to mint specified tokens and assign them the address
         return true;
     }
 }
