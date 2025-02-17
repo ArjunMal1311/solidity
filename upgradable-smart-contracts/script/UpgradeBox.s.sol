229c988c23ceaee228e3bf8c42335aa6916374e6
Arjun Malhotra <hiarjun.malhotra2002@gmail.com>
2025-02-17 16:16:28 +0530

    UpgradeBox fix

--- upgradable-smart-contracts/script/UpgradeBox.s.sol
+++ upgradable-smart-contracts/script/UpgradeBox.s.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract DeployBox is Script {
+contract UpgradeBox is Script {
     function run() external returns (address) {
         address proxy = deployBox();
         return proxy;
@@ -18,5 +20,12 @@ contract DeployBox is Script {
         BoxV1(address(proxy)).initialize();
         vm.stopBroadcast();
         return address(proxy);
+    }
+
+    function upgradeBox(address proxyAddress, address newImplementation) public returns (address) {
+        vm.startBroadcast();
+        BoxV1(proxyAddress).upgradeToAndCall(newImplementation, "");
+        vm.stopBroadcast();
+        return proxyAddress;
     }
 }
